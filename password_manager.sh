26e2e6c58710c7060148fe52ff3d96b1d83b54d2
sigehiro <sigehiro.0728@gmail.com>
2024-01-19 19:06:06 -0500

    if文で３パターンの分岐を記載

--- password_manager.sh
+++ password_manager.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
 echo "PassWordマネージャーへようこそ！"
+echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
 
-read -p "enter service name:" service_name
-#!入力値をファイルに保存
-echo "$service_name">>saved_password.txt
+read choice
 
-read -p "enter user name: " name
-echo "$name">>saved_password.txt
 
-read -p "enter password: " password
-echo "$password">>saved_password.txt
+if [ "$choice" == "Add Password" ]; then
+    read -p "enter service name:" service_name
+    #!入力値をファイルに保存
+    echo "$service_name">>saved_password.txt
 
-echo "Thank you!"
+    read -p "enter user name: " name
+    echo "$name">>saved_password.txt
+
+    read -p "enter password: " password
+    echo "$password">>saved_password.txt
+
+    echo "パスワードの追加は成功しました。"
+
+#!elif [ "$choice" == "Get Password" ]; then
+    #!処理あとから書く。
+
+elif [ "$choice" == "Exit" ]; then
+    echo "Thank you!"
+    exit 1
+
+else
+    echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+    exit 1
+
+fi
+
